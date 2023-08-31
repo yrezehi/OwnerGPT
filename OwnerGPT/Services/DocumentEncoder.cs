f9140027310bea560c34c01cdca52572ec092a97
unknown <yrezehi@gmail.com>
2023-08-30 22:17:45 -0700

    add: document encoder

--- OwnerGPT/Services/DocumentEncoder.cs
+++ OwnerGPT/Services/DocumentEncoder.cs
@@ -5,5 +5,27 @@ namespace OwnerGPT.Services
     public class DocumentEncoder
     {
         private readonly SentenceEncoder SentenceEncoder;
+
+        public DocumentEncoder(SentenceEncoder sentenceEncoder)
+        {
+            SentenceEncoder = sentenceEncoder;
+        }
+
+        public float[][] Encode(params string[] documents)
+        {
+            return SentenceEncoder.Encode(documents);
+        }
+
+        public float[] Encode(string document)
+        {
+            var encodedDocuments = SentenceEncoder.Encode(new string[] { document });
+
+            if(encodedDocuments.Length == 0)
+            {
+                throw new Exception();
+            }
+
+            return encodedDocuments[0];
+        }
     }
 }
