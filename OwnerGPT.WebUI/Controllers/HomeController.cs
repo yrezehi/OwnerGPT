3b50c9ec9ad037376530f10a0e32681296a1f0f9
unknown <yrezehi@gmail.com>
2023-09-01 09:24:34 -0700

    remove: removed layout unneeded stuff

--- OwnerGPT.WebUI/Controllers/HomeController.cs
+++ OwnerGPT.WebUI/Controllers/HomeController.cs
@@ -6,19 +6,10 @@ namespace OwnerGPT.WebUI.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        public HomeController() { }
 
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
