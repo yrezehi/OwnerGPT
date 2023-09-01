cd896bc0e6630769009bbdd6d548a84824ae26e7
unknown <yrezehi@gmail.com>
2023-09-01 10:52:07 -0700

    add: agents service

---
+++ OwnerGPT/Services/Abstract/AgentsService.cs
@@ -0,0 +1,10 @@
+﻿using OwnerGPT.Models.Agent;
+using OwnerGPT.Repositores.RDBMS.Abstracts.Interfaces;
+
+namespace OwnerGPT.Services.Abstract
+{
+    public class AgentsService : RDBMSServiceBase<Agent>
+    {
+        public AgentsService(IRDBMSUnitOfWork unitOfWork) : base(unitOfWork) { }
+    }
+}
