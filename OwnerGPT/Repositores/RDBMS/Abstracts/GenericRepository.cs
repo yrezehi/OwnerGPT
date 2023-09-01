ccf16b709140c01cca235913ded28d90897fb31c
unknown <yrezehi@gmail.com>
2023-09-01 09:48:41 -0700

    add: rdbms abstract

---
+++ OwnerGPT/Repositores/RDBMS/Abstracts/GenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OwnerGPT.Repositores.RDBMS.Abstracts.Interfaces;
+
+namespace Callem.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly DbContext Context;
+        public  DbSet<T> DBSet { get; }
+
+        public GenericRepository(DbContext context)
+        {
+            Context = context;
+            DBSet = Context.Set<T>();
+        }
+
+        public void Dispose() => throw new NotImplementedException();
+    }
+}
---
+++ OwnerGPT/Repositores/RDBMS/Abstracts/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using Callem.Repositories;
+using Microsoft.EntityFrameworkCore;
+using OwnerGPT.Repositores.RDBMS.Abstracts.Interfaces;
+
+namespace Hisuh.Repositories
+{
+    public class UnitOfWork<TContext> : IUnitOfWork<TContext>, IUnitOfWork where TContext : DbContext, IDisposable
+    {
+        public TContext Context { get; }
+
+        public UnitOfWork(TContext context) => Context = context;
+
+        public async Task<int> CompletedAsync() => await Context.SaveChangesAsync();
+
+        public async Task DisposeAsync() => await Context.DisposeAsync();
+
+        public void Dispose() => Context.Dispose();
+
+        public IGenericRepository<TEntity> Repository<TEntity>() where TEntity : class => new GenericRepository<TEntity>(Context);
+
+    }
+}
