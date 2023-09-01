8ba90e18af3be6da993834b03b92cb343a912df7
unknown <yrezehi@gmail.com>
2023-09-01 10:46:12 -0700

    add: paginatedto

---
+++ OwnerGPT/Models/DTO/PaginateDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace OwnerGPT.Models.DTO
+{
+    public class PaginateDTO<T> where T : class
+    {
+        public int Page { get; set; }
+        public int Total { get; set; }
+        public int Pages { get; set; }
+        public IEnumerable<T> Items { get; set; }
+    }
+}
