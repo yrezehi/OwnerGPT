e44fca69e3890ef82119ae1e507cceb5ec52e318
unknown <yrezehi@gmail.com>
2023-09-01 12:05:41 -0700

    ehnace: core project to api based

--- OwnerGPT/Program.cs
+++ OwnerGPT/Program.cs
@@ -1,6 +1,5 @@
 using OwnerGPT.Utilities;
 using OwnerGPT.Services;
-using OwnerGPT.DocumentEncoder.Encoder;
 using OwnerGPT.Repositores.PGVDB;
 using Microsoft.EntityFrameworkCore;
 using OwnerGPT.Repositores.RDBMS;
@@ -11,37 +10,33 @@ var builder = WebApplication.CreateBuilder(args);
 ConfigurationUtil.Initialize(builder.Configuration);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<RDBMSGenericRepositoryContext>(option => option.UseInMemoryDatabase(ConfigurationUtil.GetValue<string>("INMEMEORYDBNAME")));
 
 builder.Services.AddTransient(typeof(PGVUnitOfWork), typeof(PGVUnitOfWork));
 builder.Services.AddTransient(typeof(VectorEmbeddingService), typeof(VectorEmbeddingService));
 
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     // to trigger onModelCreate
     using (var scope = app.Services.CreateScope())
         using (var context = scope.ServiceProvider.GetService<RDBMSGenericRepositoryContext>())
             context!.Database.EnsureCreated();
 
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllers();
 
 app.Run();
