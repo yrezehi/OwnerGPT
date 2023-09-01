110b2b774426262d97ac8c06bf887ea1e7aa6715
unknown <yrezehi@gmail.com>
2023-09-01 10:59:34 -0700

    fix: agents controller

--- OwnerGPT/Controllers/AgentsController.cs
+++ OwnerGPT/Controllers/AgentsController.cs
@@ -1,11 +1,12 @@
 ﻿using OwnerGPT.Controllers.Abstract;
 using OwnerGPT.Models.Agents;
+using OwnerGPT.Models.DTO;
 using OwnerGPT.Services.Abstract;
 
 namespace OwnerGPT.Controllers
 {
-    public class AgentsController : BaseController<AgentsService, Agent, GenreDTO>
+    public class AgentsController : BaseController<AgentsService, Agent, AgentDTO>
     {
-        public GenresController(GenresService Service) : base(Service) { }
+        public AgentsController(AgentsService Service) : base(Service) { }
     }
 }
