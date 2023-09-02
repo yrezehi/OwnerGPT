45ba689c02f1d068db780bf0e227d4add46cc365
unknown <yrezehi@gmail.com>
2023-09-02 06:19:05 -0700

    ui: chat ui

--- OwnerGPT.WebUI.Admin/tailwind.config.js
+++ OwnerGPT.WebUI.Admin/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./Views/**/*.cshtml'],
+    content: ['./Views/**/*.cshtml', './node_modules/flowbite/**/*.js'],
     darkMode: false,
     mode: 'jit',
     theme: {
