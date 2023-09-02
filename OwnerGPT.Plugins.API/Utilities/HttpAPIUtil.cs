670b2ed91fa73debc757320d63c07bb147a7ab83
unknown <yrezehi@gmail.com>
2023-09-02 10:04:37 -0700

    add: http api util

---
+++ OwnerGPT.Plugins.API/Utilities/HttpAPIUtil.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace OwnerGPT.Plugins.Parsers.API.Utilities
+{
+    public class HttpAPIUtil
+    {
+        private readonly IHttpClientFactory HttpClientFactory;
+
+        public HttpAPIUtil(IHttpClientFactory httpClientFactory) {
+            HttpClientFactory = httpClientFactory;
+        }
+
+        public HttpClient GetClientInstance() =>
+            HttpClientFactory.CreateClient();
+        
+        public async Task<T> GetAsync<T>(string endpoint)
+        {
+            return Deserialize<T>(await (await GetClientInstance().GetAsync(endpoint)).Content.ReadAsStringAsync());
+        }
+
+        public async Task<T> PostAsync<T, B>(string endpoint, B body)
+        {
+            return Deserialize<T>(await (await GetClientInstance().PostAsJsonAsync(endpoint, body)).Content.ReadAsStringAsync());
+        }
+
+        private T Deserialize<T>(string json)
+        {
+            return JsonSerializer.Deserialize<T>(json)!;
+        }
+
+    }
+}
