78b3ce49f34d3c45a7cf83ff2794e1483a363489
unknown <yrezehi@gmail.com>
2023-09-02 08:58:10 -0700

    ui: font

---
+++ OwnerGPT.WebUI.Public/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OwnerGPT.WebUI.Public.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
