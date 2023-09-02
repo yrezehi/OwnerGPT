d42a89879dfcc1643a8d4efa59e705f20adf5b31
unknown <yrezehi@gmail.com>
2023-09-01 23:26:39 -0700

    add: plugin config reader

---
+++ OwnerGPT.Plugins.Manager/Configuration/PluginConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+
+namespace OwnerGPT.Plugins.Manager.Configuration
+{
+    public class PluginConfiguration
+    {
+        public List<string> Plugins { get; set; }
+
+        public static PluginConfiguration LoadFile(string configurationPath)
+        {
+            using (StreamReader reader = new StreamReader(configurationPath))
+            {
+                return JsonSerializer.Deserialize<PluginConfiguration>(reader.ReadToEnd())!;
+            }
+        }
+    }
+}
--- OwnerGPT.Plugins.Manager/Manager/PluginManager.cs
+++ OwnerGPT.Plugins.Manager/Manager/PluginManager.cs
@@ -1,16 +1,20 @@
-﻿using System.Reflection;
+﻿using OwnerGPT.Plugins.Manager.Configuration;
+using System.Reflection;
 
 namespace OwnerGPT.Plugins.Manager.Manager
 {
     public class PluginManager
     {
         public IList<string> PluginAssemblies;
+        
+        public PluginConfiguration PluginConfiguration;
 
         private static string PLUGIN_CONFIGURATION_PATH = "plugins.json";
 
         public PluginManager()
         {
             PluginAssemblies = new List<string>();
+            PluginConfiguration = PluginConfiguration.LoadFile(PLUGIN_CONFIGURATION_PATH);
         }
 
         public string GetAssemblyPathByName(string assemblyName)
@@ -18,17 +22,5 @@ namespace OwnerGPT.Plugins.Manager.Manager
             return Path.Combine(Directory.GetParent(Assembly.GetEntryAssembly()!.Location)!.FullName, assemblyName + ".dll");
         }
 
-        public string[] GetPluginNames()
-        {
-            using(StreamReader reader = new StreamReader(PLUGIN_CONFIGURATION_PATH))
-            {
-
-            }
-        }
-
-        private string[] LoadPluginConfigurationFile()
-        {
-
-        }
     }
 }
