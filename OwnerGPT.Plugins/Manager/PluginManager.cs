528d777043d78f43dcddca8de0b8bf429f4159d6
unknown <yrezehi@gmail.com>
2023-09-01 22:23:52 -0700

    setup: plugin managert

--- OwnerGPT.Plugins/Manager/PluginManager.cs
+++ OwnerGPT.Plugins/Manager/PluginManager.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OwnerGPT.Plugins.Manager
+﻿namespace OwnerGPT.Plugins.Manager
 {
-    internal class PluginManager
+    public class PluginManager
     {
+        public IList<string> PluginAssemblies;
+
+        public PluginManager()
+        {
+            PluginAssemblies = new List<string>();
+        }
     }
 }
--- OwnerGPT/Configuration/OwnerGPTExtensions.cs
+++ OwnerGPT/Configuration/OwnerGPTExtensions.cs
@@ -1,4 +1,7 @@
-﻿using OwnerGPT.Utilities;
+﻿using Microsoft.EntityFrameworkCore;
+using OwnerGPT.Repositores.PGVDB;
+using OwnerGPT.Repositores.RDBMS;
+using OwnerGPT.Utilities;
 
 namespace OwnerGPT.Configuration
 {
@@ -11,6 +14,25 @@ namespace OwnerGPT.Configuration
 
             // Inject configuration into static object
             ConfigurationUtil.Initialize(builder.Configuration);
+
+            return builder;
+        }
+
+        public static WebApplicationBuilder RegisterPGVDB(this WebApplicationBuilder builder)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            builder.Services.AddTransient(typeof(PGVUnitOfWork), typeof(PGVUnitOfWork));
+            builder.Services.AddSingleton(typeof(PGVUnitOfWorkInMemeory), typeof(PGVUnitOfWorkInMemeory));
+
+            return builder;
+        }
+
+        public static WebApplicationBuilder RegisterRDBMS(this WebApplicationBuilder builder)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            builder.Services.AddDbContext<RDBMSGenericRepositoryContext>(option => option.UseInMemoryDatabase(ConfigurationUtil.GetValue<string>("INMEMEORYDBNAME")));
 
             return builder;
         }
