4abb753b6b4b9db2246e527d778bd73d2166b94d
unknown <yrezehi@gmail.com>
2023-09-02 10:43:34 -0700

    setup: account controller

---
+++ OwnerGPT.WebUI.Admin/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OwnerGPT.WebUI.Admin.Models;
+using System.Diagnostics;
+
+namespace OwnerGPT.WebUI.Admin.Controllers
+{
+    [Route("[controller]")]
+    public class AccountController : Controller
+    {
+        [HttpGet("[Action]")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
