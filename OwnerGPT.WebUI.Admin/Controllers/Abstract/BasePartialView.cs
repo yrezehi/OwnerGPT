f1c03215672d06f4890770fa23900ed5d9256a3a
unknown <yrezehi@gmail.com>
2023-09-04 05:59:48 -0700

    base partial view

---
+++ OwnerGPT.WebUI.Admin/Controllers/Abstract/BasePartialView.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OwnerGPT.WebUI.Admin.Controllers.Abstract
+{
+    public class BasePartialView
+    {
+        public IActionResult PartialView(string viewName) => PartialView(viewName);
+    }
+}
