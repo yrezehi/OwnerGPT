9ab19a66b420fc0d7315c4324ef765aa3b11ee03
unknown <yrezehi@gmail.com>
2023-09-04 11:32:54 -0700

    setup: js loading

---
+++ OwnerGPT.WebUI.Admin/wwwroot/js/loading.js
@@ -0,0 +1,10 @@
+﻿
+var loading = function () {
+
+    return function () {
+        return Object.freeze({
+            unload: function () { },
+            load: function () { },
+        });
+    }();
+}();
