64cfc2251afc344b345c4800e6167fd6908bf40b
unknown <yrezehi@gmail.com>
2023-09-04 10:02:36 -0700

    add: base fetch request util

---
+++ OwnerGPT.WebUI.Admin/wwwroot/js/fetch.js
@@ -0,0 +1,24 @@
+﻿
+var fetchRequest = function () {
+    function handleError() { }
+
+    function objectToQueryParameters() { }
+
+    return function () {
+
+        async function getRequest() { }
+
+        async function postRequest() { }
+
+        async function deleteRequest() { }
+
+        async function putRequest() { }
+
+        return Object.freeze({
+            getRequest,
+            postRequest,
+            deleteRequest,
+            putRequest
+        });
+    }();
+}();
