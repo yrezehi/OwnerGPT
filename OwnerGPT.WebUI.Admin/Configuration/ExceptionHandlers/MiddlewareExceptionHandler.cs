d4727f2f3f226ee9ff2bd41a5444d9602461787b
unknown <yrezehi@gmail.com>
2023-09-04 09:51:14 -0700

    add: global error handler

---
+++ OwnerGPT.WebUI.Admin/Configuration/ExceptionHandlers/MiddlewareExceptionHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.Json;
+
+namespace WebApplication1.ExceptionHandlers.Configuration
+{
+    public class MiddlewareExceptionHandler
+    {
+
+        private readonly RequestDelegate _next;
+        private readonly ILogger<MiddlewareExceptionHandler> _logger;
+
+        public MiddlewareExceptionHandler(RequestDelegate next, ILogger<MiddlewareExceptionHandler> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try { await _next(context); }
+            catch (Exception exception) { await HandleExceptionAsync(context, exception, _logger); }
+        }
+
+        private static async Task HandleExceptionAsync(
+            HttpContext context,
+            Exception exception,
+            ILogger<MiddlewareExceptionHandler> logger)
+        {
+
+            // To help troubleshoot the issue, trace id will be shown to the user
+            string traceId = context.TraceIdentifier.ToString();
+            int statusCode = 500;
+
+            // Show custom error thrown by the developer or Internal Error Message if unhandled error
+            string? exceptionMessage = "Internal Server Error";
+
+
+            // Object holds the error details that will be logged in the server
+            dynamic loggingErrorObject = new
+            {
+                StatusCode = statusCode,
+                Message = exception.Message,
+                StackTrace = exception.StackTrace,
+                InnerException = exception.InnerException,
+                Cause = "Unhandled exception",
+                TraceId = traceId
+            };
+
+            // Convert logging error object to string and log it
+            string serializedErrorObject = JsonSerializer.Serialize(loggingErrorObject);
+            logger.LogError(serializedErrorObject);
+            context.Response.Redirect($"/Home/error?message=" + exceptionMessage + "&traceid=" + traceId);
+        }
+    }
+}
