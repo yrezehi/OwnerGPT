4eca8d435c422c0096cb55ae76864b846d703ca6
unknown <yrezehi@gmail.com>
2023-09-04 10:32:40 -0700

    setup: excel plugin parser

---
+++ OwnerGPT.Plugins.Parsers.Excel/ExcelPlugin.cs
@@ -0,0 +1,12 @@
+﻿using OwnerGPT.Plugins.Manager.Interfaces;
+
+namespace OwnerGPT.Plugins.Parsers.Excel
+{
+    public class ExcelPlugin : IOwnerGPTParserPlugin<string, string>
+    {
+        public Task<string> Process(string content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- OwnerGPT.WebUI.Widget/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace OwnerGPT.WebUI.Widget.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
