d78d2768d15148a4d094855567c6770464ec3b6d
unknown <yrezehi@gmail.com>
2023-09-04 07:14:44 -0700

    add: notification.js

---
+++ OwnerGPT.WebUI.Admin/wwwroot/js/notification.js
@@ -0,0 +1,41 @@
+﻿
+var notification = function () {
+    var notificationIstance = getNotificationIstance();
+
+    function getNotificationIstance() {
+        return new Polipop('polipop', {
+            layout: 'popups',
+            insert: 'before',
+            life: 5000,
+            icons: false,
+            closer: false
+        });
+    }
+
+    function spawnNotification(content, title, type) {
+        notificationIstance.add(
+            {
+                content: content,
+                title: title,
+                type: type,
+            }
+        );
+    }
+
+    return function () {
+        return Object.freeze({
+            error: function (title, content) {
+                spawnNotification(title, content, "error");
+            },
+            information: function (title, content) {
+                spawnNotification(title, content, "info");
+            },
+            warning: function (title, content) {
+                spawnNotification(title, content, "warning");
+            },
+            success: function (title, content) {
+                spawnNotification(title, content, "success");
+            }
+        });
+    }();
+}();
