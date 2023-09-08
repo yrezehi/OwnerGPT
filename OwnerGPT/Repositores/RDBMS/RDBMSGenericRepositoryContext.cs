9e4eeb5eb9ca05b04036fe7babec456888b9d99d
unknown <yrezehi@gmail.com>
2023-09-08 09:40:18 -0700

    add: seed agent

--- OwnerGPT/Repositores/RDBMS/RDBMSGenericRepositoryContext.cs
+++ OwnerGPT/Repositores/RDBMS/RDBMSGenericRepositoryContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OwnerGPT.Models.Agents;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace OwnerGPT.Repositores.RDBMS
 {
@@ -6,6 +8,23 @@ namespace OwnerGPT.Repositores.RDBMS
     {
         public RDBMSGenericRepositoryContext(DbContextOptions options) : base(options) { }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Agent>().HasData(
+                new Agent
+                {
+                    Id = 1,
+                    CreationDate = DateTime.Now,
+                    Description = "External Portal Chatbot",
+                    Name = "Agent #1",
+                },
+                new Agent
+                {
+                    Id = 1,
+                    CreationDate = DateTime.Now,
+                    Description = "Internal Portal Chatbot",
+                    Name = "Agent #2",
+                }
+            );
+        }
     }
 }
