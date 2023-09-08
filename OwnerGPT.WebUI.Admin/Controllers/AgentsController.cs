acade375002575d07be6f3e3bea30b8f06bfb499
unknown <yrezehi@gmail.com>
2023-09-08 09:30:25 -0700

    setup: basic functions agents controller

--- OwnerGPT.WebUI.Admin/Controllers/AgentsController.cs
+++ OwnerGPT.WebUI.Admin/Controllers/AgentsController.cs
@@ -1,11 +1,30 @@
-﻿using OwnerGPT.Controllers.Abstract;
+﻿using Microsoft.AspNetCore.Mvc;
+using OwnerGPT.Controllers.Abstract;
 using OwnerGPT.Models.Agents;
+using OwnerGPT.Models.Bindings;
 using OwnerGPT.Services;
 
 namespace OwnerGPT.WebUI.Admin.Controllers
 {
     public class AgentsController : RDBMSBaseController<AgentsService, Agent>
     {
-        public AgentsController(AgentsService service) : base(service) { }
+        public AgentsController(AgentsService Service) : base(Service) { }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await Service.GetAll());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromForm][Bind(AgentBinding.Create)] Agent agent)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.ValidationState);
+            }
+
+            return Ok(await Service.Create(agent));
+        }
     }
 }
