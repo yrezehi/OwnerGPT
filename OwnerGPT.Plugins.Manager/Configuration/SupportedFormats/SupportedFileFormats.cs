8ee0160b1192a8db1d491854b6b8b519813bcb88
unknown <yrezehi@gmail.com>
2023-09-08 09:24:46 -0700

    add: agents controller webui.admin

--- OwnerGPT.Plugins.Manager/Configuration/SupportedFormats/SupportedFileFormats.cs
+++ OwnerGPT.Plugins.Manager/Configuration/SupportedFormats/SupportedFileFormats.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OwnerGPT.Plugins.Manager.Configuration.SupportedFormats
+﻿namespace OwnerGPT.Plugins.Manager.Configuration.SupportedFormats
 {
     public enum SupportedFileFormats
     {
-        Pdf,
-        Text
+        PDF,
+        TEXT
     }
 }
---
+++ OwnerGPT.WebUI.Admin/Controllers/AgentsController.cs
@@ -0,0 +1,11 @@
+﻿using OwnerGPT.Controllers.Abstract;
+using OwnerGPT.Models.Agents;
+using OwnerGPT.Services;
+
+namespace OwnerGPT.WebUI.Admin.Controllers
+{
+    public class AgentsController : RDBMSBaseController<AgentsService, Agent>
+    {
+        public AgentsController(AgentsService service) : base(service) { }
+    }
+}
