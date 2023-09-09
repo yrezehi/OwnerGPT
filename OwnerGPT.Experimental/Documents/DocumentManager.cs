c0bb0778b61cb140c4be602bf4e00bd559b2e775
unknown <yrezehi@gmail.com>
2023-09-09 09:18:16 -0700

    setup: document manager for plugin manager

---
+++ OwnerGPT.Experimental/Documents/DocumentManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnerGPT.Experimental.Documents
+{
+    public class DocumentManager
+    {
+        private static string TEMPORARY_FILE_PATH = "C:\\OwnerGPT.Temporary.Upload";
+
+        public DocumentManager() { 
+
+        }
+    }
+}
--- OwnerGPT.Plugins.WEB/Utilities/ScraperUtil.cs
+++ OwnerGPT.Plugins.WEB/Utilities/ScraperUtil.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OwnerGPT.Plugins.Parsers.WEB.Utilities
+﻿namespace OwnerGPT.Plugins.Parsers.WEB.Utilities
 {
     // TODO: set proper agent
     // TODO: everything is dynamic nowdays ahhh
