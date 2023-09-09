df4abf1c16de1c37fe2e0fb77fcdbb250698ae22
unknown <yrezehi@gmail.com>
2023-09-09 09:37:15 -0700

    setup: MagicalDocument

---
+++ OwnerGPT.Experimental/Documents/MagicalDocument.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnerGPT.Experimental.Documents
+{
+    public class MagicalDocument
+    {
+        public void MapDocumentTypeToPlugin()
+        {
+
+        }
+    }
+}
