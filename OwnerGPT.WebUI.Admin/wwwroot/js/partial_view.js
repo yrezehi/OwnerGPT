63d0dbbcad69e4c0a76d657e92db35a824396d29
unknown <yrezehi@gmail.com>
2023-09-09 21:34:40 -0700

    edit: rename partial view file and ecapsolate it

--- OwnerGPT.WebUI.Admin/wwwroot/js/partial_view.js
+++
@@ -1,8 +0,0 @@
-﻿
-function appendPartialView(viewName, parameter, context) {
-    fetch(`/GetPartialView?viewName=${viewName}&parameter=${parameter}`)
-        .then(response => response.text())
-        .then(result => {
-            context.innerHTML += result;
-        });
-}
---
+++ OwnerGPT.WebUI.Admin/wwwroot/js/partial_view_loader.js
@@ -0,0 +1,20 @@
+﻿
+var partialViewLoader = function () {
+
+    function appendTo(viewName, parameter, context) {
+        fetch(`/PartialView?viewName=${viewName}&parameter=${parameter}`)
+            .then(response => response.text())
+            .then(result => {
+                context.innerHTML += result;
+        });
+    }
+
+    return function () {
+        return Object.freeze({
+            setElement: function () { },
+            unsetElement: function () { },
+            setSVG: function () { },
+            unsetSVG: function () { }
+        });
+    }();
+}();
