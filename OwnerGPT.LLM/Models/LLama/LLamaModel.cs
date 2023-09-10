1fdf9be616040a1d18a9076063bc28adba881715
unknown <yrezehi@gmail.com>
2023-09-10 04:50:06 -0700

    update: llamasharp

--- OwnerGPT.LLM/Models/LLama/LLamaModel.cs
+++ OwnerGPT.LLM/Models/LLama/LLamaModel.cs
@@ -3,27 +3,26 @@ using LLama.Common;
 using OwnerGPT.LLM.Configuration;
 using OwnerGPT.LLM.Interfaces;
 using System.Collections;
+using System.Reflection;
 using System.Text;
 
 namespace OwnerGPT.LLM.Models.LLama
 {
     public class LLAMAModel : ILLMModel
     {
-        private LLamaModel Model { get; set; }
-
-        private StatelessExecutor Executor { get; set; }
+        private InteractiveExecutor Executor { get; set; }
         private InferenceParams InferenceParams { get; set; }
 
         public LLAMAModel()
         {
-            Model = new LLamaModel(new ModelParams(
-                ModelConfiguration.LLAMA_MODEL_PATH,
-                contextSize: ModelConfiguration.MODEL_CONTEXT_SIZE,
-                seed: ModelConfiguration.MODEL_SEED_COUNT,
-                gpuLayerCount: ModelConfiguration.MODEL_GPU_LAYER_COUNT
-            ));
+            var parameters = new ModelParams(ModelConfiguration.LLAMA_MODEL_PATH)
+            {
+                ContextSize = ModelConfiguration.MODEL_CONTEXT_SIZE,
+                Seed = ModelConfiguration.MODEL_SEED_COUNT,
+                GpuLayerCount = ModelConfiguration.MODEL_GPU_LAYER_COUNT
+            };
 
-            Executor = new StatelessExecutor(Model);
+            Executor = new InteractiveExecutor(LLamaWeights.LoadFromFile(parameters).CreateContext(parameters));
 
             InferenceParams = new InferenceParams
             {
