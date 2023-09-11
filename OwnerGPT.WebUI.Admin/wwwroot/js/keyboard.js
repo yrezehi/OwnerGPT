600b2142ac4601768a40f078a7fc3ccc1bd0c2f7
unknown <yrezehi@gmail.com>
2023-09-11 07:45:04 -0700

    add: keyboard.js

---
+++ OwnerGPT.WebUI.Admin/wwwroot/js/keyboard.js
@@ -0,0 +1,25 @@
+﻿var keyboard = function () {
+
+    function bindEnterToSubmit(event, context) {
+        if (event.which === 13) {
+            if (!event.repeat) {
+                context.closest("form").submit();
+            }
+            event.preventDefault();
+        }
+    }
+
+    function bindEventLisnter(context, event, callback) {
+        context.addEventListener(event, callback);
+    }
+
+    return function () {
+        return Object.freeze({
+            enterToSubmit: function (context) {
+                bindEventLisnter(context, "keydown", function (event) {
+                    bindEnterToSubmit(event, context);
+                });
+            },
+        });
+    }();
+}();
