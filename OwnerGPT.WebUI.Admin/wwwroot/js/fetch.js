4b07a7a106826e41889ce46c06db11a182663f41
unknown <yrezehi@gmail.com>
2023-09-11 06:45:55 -0700

    fix: minor fix and added notification to error handler

--- OwnerGPT.WebUI.Admin/wwwroot/js/fetch.js
+++ OwnerGPT.WebUI.Admin/wwwroot/js/fetch.js
@@ -1,20 +1,24 @@
 ﻿
 var fetchRequest = function () {
+
     const buildSuccessResponse = function (status, data) {
         return { success: true, status: status, data: data };
     };
-    const buildErrorResponse = function (error) {
+    const handleAndBuildErrorResponse = function (error) {
+        notification.error("Something Went Wrong!");
         return { success: false, error: error };
     };
+
     return function () {
+
         async function getRequest(endpoint) {
             return fetch(endpoint, {
                 method: 'GET',
             }).then(response => response.json()
             .then((data) => buildSuccessResponse(response.status, data)))
-            .catch(error => buildErrorResponse(error));
+                .catch(error => handleAndBuildErrorResponse(error));
         }
-        async function postRequest(endpoint) {
+        async function postRequest(endpoint, body) {
             return fetch(endpoint, {
                 method: 'POST',
                 headers: {
@@ -23,16 +27,16 @@ var fetchRequest = function () {
                 body: JSON.stringify(body),
             }).then(response => response.json()
             .then((data) => { return buildSuccessResponse(response.status, data) }))
-            .catch(error => buildErrorResponse(error));
+                .catch(error => handleAndBuildErrorResponse(error));
         }
         async function deleteRequest(endpoint, id) {
             return fetch(endpoint + id, {
                 method: 'GET',
             }).then(response => response.json())
             .then((data) => buildSuccessResponse(response.status, data))
-            .catch(error => buildErrorResponse(error));
+                .catch(error => handleAndBuildErrorResponse(error));
         }
-        async function putRequest(endpoint) {
+        async function putRequest(endpoint, body) {
             return fetch(endpoint, {
                 method: 'PUT',
                 headers: {
@@ -41,8 +45,9 @@ var fetchRequest = function () {
                 body: JSON.stringify(body),
             }).then(response => response.json())
             .then((data) => buildSuccessResponse(response.status, data))
-            .catch(error => buildErrorResponse(error));
+                .catch(error => handleAndBuildErrorResponse(error));
         }
+
         return Object.freeze({
             getRequest,
             postRequest,
