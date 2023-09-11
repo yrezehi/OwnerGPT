e565b584857377758a79f8430849bba6e7fe84f5
unknown <yrezehi@gmail.com>
2023-09-11 06:43:50 -0700

    add: generic restful request

--- OwnerGPT.WebUI.Admin/wwwroot/js/fetch.js
+++ OwnerGPT.WebUI.Admin/wwwroot/js/fetch.js
@@ -1,19 +1,48 @@
 ﻿
 var fetchRequest = function () {
-    function handleError() { }
-
-    function objectToQueryParameters() { }
-
+    const buildSuccessResponse = function (status, data) {
+        return { success: true, status: status, data: data };
+    };
+    const buildErrorResponse = function (error) {
+        return { success: false, error: error };
+    };
     return function () {
-
-        async function getRequest() { }
-
-        async function postRequest() { }
-
-        async function deleteRequest() { }
-
-        async function putRequest() { }
-
+        async function getRequest(endpoint) {
+            return fetch(endpoint, {
+                method: 'GET',
+            }).then(response => response.json()
+            .then((data) => buildSuccessResponse(response.status, data)))
+            .catch(error => buildErrorResponse(error));
+        }
+        async function postRequest(endpoint) {
+            return fetch(endpoint, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(body),
+            }).then(response => response.json()
+            .then((data) => { return buildSuccessResponse(response.status, data) }))
+            .catch(error => buildErrorResponse(error));
+        }
+        async function deleteRequest(endpoint, id) {
+            return fetch(endpoint + id, {
+                method: 'GET',
+            }).then(response => response.json())
+            .then((data) => buildSuccessResponse(response.status, data))
+            .catch(error => buildErrorResponse(error));
+        }
+        async function putRequest(endpoint) {
+            return fetch(endpoint, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(body),
+            }).then(response => response.json())
+            .then((data) => buildSuccessResponse(response.status, data))
+            .catch(error => buildErrorResponse(error));
+        }
         return Object.freeze({
             getRequest,
             postRequest,
