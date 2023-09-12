3779004daaab6c7b4c47932a21a3ac3e38708819
unknown <yrezehi@gmail.com>
2023-09-12 07:22:26 -0700

    add: threadjob

---
+++ OwnerGPT.Core/Threads/ThreadJob.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnerGPT.Core.Threads
+{
+    public class ThreadJob
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationTime { get; set; }
+        public DateTime StartTime { get; set; }
+    }
+}
