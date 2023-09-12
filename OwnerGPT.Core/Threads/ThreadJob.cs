8dc3e5d03db04f681d75c9b95e77182fc439fdac
unknown <yrezehi@gmail.com>
2023-09-12 07:30:31 -0700

    add: trigger thread

--- OwnerGPT.Core/Threads/ThreadJob.cs
+++ OwnerGPT.Core/Threads/ThreadJob.cs
@@ -14,7 +14,7 @@ namespace OwnerGPT.Core.Threads
         public DateTime StartTime { get; set; }
         public Thread Job { get; set; }
 
-        public ThreadJob(ParameterizedThreadStart jobAction, string description)
+        public ThreadJob(ParameterizedThreadStart jobAction, string description, bool triggerImmediately = false)
         {
             Job = new Thread(jobAction);
 
@@ -22,6 +22,21 @@ namespace OwnerGPT.Core.Threads
             CreationTime = DateTime.Now;
 
             Description = description;
+
+            if (triggerImmediately)
+            {
+                Job.Start();
+                StartTime = DateTime.Now;
+            }
+        }
+
+        public void Trigger()
+        {
+            if (Job.ThreadState == ThreadState.Unstarted)
+            {
+                StartTime = DateTime.Now;
+                Job.Start();
+            }
         }
 
     }
