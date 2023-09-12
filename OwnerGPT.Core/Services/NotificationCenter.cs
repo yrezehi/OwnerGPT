94bc486f0993dde8038119caef2f57bc71b25617
unknown <yrezehi@gmail.com>
2023-09-12 08:47:43 -0700

    setup: notification

---
+++ OwnerGPT.Core/Services/NotificationCenter.cs
@@ -0,0 +1,30 @@
+﻿using OwnerGPT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnerGPT.Core.Services
+{
+    public class NotificationCenter
+    {
+
+        public NotificationCenter() { }
+
+        public Notification GetUnreadNotificationsFor(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PushNotificationToAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PushNotificationToTo(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
