2d1f2e895e85e081a682798d0095a65f59020042
unknown <yrezehi@gmail.com>
2023-09-12 08:24:01 -0700

    setup: thread manager

---
+++ OwnerGPT.Core/Threads/ThreadJobManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnerGPT.Core.Threads
+{
+    public class ThreadJobManager
+    {
+        public readonly Queue<ThreadJob> Queue;
+
+        public ThreadJobManager()
+        {
+            Queue = new Queue<ThreadJob>();
+        }
+
+        public bool IsAnyJobAvailable()
+        {
+            return Queue.Count > 0;
+        }
+    }
+}
