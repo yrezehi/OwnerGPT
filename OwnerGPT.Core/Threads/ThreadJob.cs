e88c4f551c9ce9f01bf803d12069087a397cccc9
unknown <yrezehi@gmail.com>
2023-09-12 07:26:56 -0700

    add: construct to threadJob

--- OwnerGPT.Core/Threads/ThreadJob.cs
+++ OwnerGPT.Core/Threads/ThreadJob.cs
@@ -12,5 +12,17 @@ namespace OwnerGPT.Core.Threads
         public string Description { get; set; }
         public DateTime CreationTime { get; set; }
         public DateTime StartTime { get; set; }
+        public Thread Job { get; set; }
+
+        public ThreadJob(ParameterizedThreadStart jobAction, string description)
+        {
+            Job = new Thread(jobAction);
+
+            Id = Job.ManagedThreadId;
+            CreationTime = DateTime.Now;
+
+            Description = description;
+        }
+
     }
 }
