d553307248630442c50ccf2c803d408609eac888
unknown <yrezehi@gmail.com>
2023-09-13 07:53:10 -0700

    setup: params for ADAuthentication

--- OwnerGPT.Core/Authentication/ADAuthentication.cs
+++ OwnerGPT.Core/Authentication/ADAuthentication.cs
@@ -1,11 +1,27 @@
-﻿using OwnerGPT.Core.Authentication.Abstracts;
+﻿using System.DirectoryServices.Protocols;
+using System.Runtime.InteropServices;
 
 namespace OwnerGPT.Core.Authentication
 {
-    public class ADAuthentication : IAuthentication
+    public class ADAuthentication
     {
-        public ADAuthentication() { }
+        private readonly string LDAP_HOST;
+        private readonly string LDAP_PORT;
+        
+        private readonly string LDAP_DOMAIN;
+        private readonly string LDAP_SUB_DOMAIN;
 
-        public bool Authenticate() { return false; }
+        private readonly string LDAP_USER;
+        private readonly string LDAP_PASSWORD;
+
+        private readonly LdapConnection Connection;
+
+        public ADAuthentication()
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                throw new PlatformNotSupportedException("Only windows is supported at the moment for active directory");
+            }
+        }
     }
 }
