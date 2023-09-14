d1915596054b0c67cfb1fd6f436ddb3578d7d7eb
unknown <yrezehi@gmail.com>
2023-09-14 10:44:10 -0700

    add: sizeToReadableString

--- OwnerGPT.WebUI.Admin/wwwroot/js/utility.js
+++ OwnerGPT.WebUI.Admin/wwwroot/js/utility.js
@@ -5,8 +5,12 @@
             shortGUID: function () {
                 return Math.random().toString(36).slice(-6);
             },
-            stringEllipsis(string, length) {
+            stringEllipsis: function (string, length) {
                 return string.substring(0, length) + "...";
+            },
+            sizeToReadableString: function (size) {
+                var carrier = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
+                return (size / Math.pow(1024, carrier)).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][carrier];
             }
         });
     }();
