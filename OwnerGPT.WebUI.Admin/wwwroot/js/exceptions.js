6cdd765c635fdbb3fe8a7b12be145353ed83086f
unknown <yrezehi@gmail.com>
2023-09-15 10:39:30 -0700

    setup: exception js

---
+++ OwnerGPT.WebUI.Admin/wwwroot/js/exceptions.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    window.onunhandledrejection = event => {
+        console.warn(`UNHANDLED PROMISE REJECTION: ${event.reason}`);
+    };
+
+    window.onerror = function (message, source, lineNumber, colno, error) {
+        console.warn(`UNHANDLED ERROR: ${error.stack}`);
+    };
+
+    function upsertError() { }
+})();
