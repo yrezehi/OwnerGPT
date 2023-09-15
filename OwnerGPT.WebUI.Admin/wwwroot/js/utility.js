b3bffc21629ee02b47dff1f2b62da5d7f159cb7e
unknown <yrezehi@gmail.com>
2023-09-15 06:10:27 -0700

    add: prompts words count limit

--- OwnerGPT.WebUI.Admin/wwwroot/js/utility.js
+++ OwnerGPT.WebUI.Admin/wwwroot/js/utility.js
@@ -11,6 +11,9 @@
             sizeToReadableString: function (size) {
                 var carrier = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
                 return (size / Math.pow(1024, carrier)).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][carrier];
+            },
+            wordCount: function(string) {
+                return string.trim().split(/\s+/).length;
             }
         });
     }();
