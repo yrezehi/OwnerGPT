82a25e9c4c60b3c6eef376c18f1385ae96eb6880
unknown <yrezehi@gmail.com>
2023-09-16 13:06:19 -0700

    setup: account status

---
+++ OwnerGPT.Models/Entities/AccountStatus.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OwnerGPT.Models.Entities
+{
+    [Table("account_status")]
+    public class AccountStatus
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
