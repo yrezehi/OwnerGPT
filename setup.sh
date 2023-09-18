848b35da27d73265ed5750cbbf00fe8c331e01e7
unknown <yrezehi@gmail.com>
2023-09-18 11:46:55 -0700

    add: curl on setup.sh

--- setup.sh
+++ setup.sh
@@ -17,4 +17,14 @@ fi
 LLAMA_MODEL_URL="https://huggingface.co/TheBloke/llama-2-7B-Guanaco-QLoRA-GGUF/resolve/main/llama-2-7b-guanaco-qlora.Q5_K_S.gguf"
 LLAMA_MODEL_FILE_NAME="llama-7b.gguf"
 
+echo -e "\n\nchecking model link...\n"
+
+if curl --output /dev/null --silent --head --fail "$LLAMA_MODEL_URL"; then
+  echo "URL of the model is working fine!"
+else
+  echo "URL does not exist or there something not right!"
+fi
+
+echo -e "\n\nattempting to download the model to ./$LLAMA_MODEL_FILE_NAME"
+
 curl -L "$LLAMA_MODEL_URL" --progress-bar > $LLAMA_MODEL_FILE_NAME
