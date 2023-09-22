fb35cbdea349e0005f7cdec8a42399e1b979a4ba
unknown <yrezehi@gmail.com>
2023-09-21 23:29:59 -0700

    setup: AgentDocumentsService

---
+++ OwnerGPT.Core/Services/AgentDocumentsService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using OwnerGPT.Core.Services.Abstract;
+using OwnerGPT.Core.Services.Abstract.Interfaces;
+using OwnerGPT.Core.Services.Compositions;
+using OwnerGPT.Databases.Repositores.RDBMS.Abstracts.Interfaces;
+using OwnerGPT.DocumentEmbedding.Encoder;
+using OwnerGPT.LLM.PromptEnginnering;
+using OwnerGPT.Models.Entities;
+using OwnerGPT.Models.Entities.Agents;
+using OwnerGPT.Models.Entities.DTO;
+using OwnerGPT.Plugins.Manager.Documents.Models;
+using OwnerGPT.Plugins.Parsers.PDF;
+
+namespace OwnerGPT.Core.Services
+{
+    public class AgentDocumentsService : RDBMSServiceBase<AgentDocument>
+    {
+
+        private readonly DocumentService DocumentService;
+
+        public AgentDocumentsService(IRDBMSUnitOfWork unitOfWork, DocumentService documentService) : base(unitOfWork){
+            DocumentService = documentService;
+        }
+    }
+}
