f5eed7c1cb487302465696e2df91414012321c04
unknown <yrezehi@gmail.com>
2023-09-22 02:14:48 -0700

    enhance: prompts

--- OwnerGPT.LLM/PromptEnginnering/Prompts.cs
+++ OwnerGPT.LLM/PromptEnginnering/Prompts.cs
@@ -2,29 +2,35 @@
 {
     public static class Prompts
     {
-        private static string SUMMARIZE = @"
-            Write a concise and comprehensive summary of:
-            <INPUT_DATA>
-            Summery:
-        ";
 
-        private static string QUESTION_ANSWER = @"
-            Answer the question based on the context below. Keep the answer short and concise. Respond ""Unsure about answer"" if not sure about the answer.
-            Context: <CONTEXT_DATA>
-            Question: <INPUT_DATA>
+        public static string ANSWER_CONTEXT = "Answer the question based on the context below";
+        public static string KEEP_ANSWER_SHORT = "Answer the question based on the context below:";
+        public static string RESPOND_UNSURE_IF_YOUR = "Respond \"Unsure about answer\" if not sure about the answer";
+
+        public static string GENERIC_ASSISTANT = @"Transcript of a dialog, where the User interacts with an Assistant named <NAME/>.
+            <NAME/> is helpful, kind, honest, good at writing, and never fails to provide short answer to User's requests immediately and with precision.";
+
+        private static string QUESTION_ANSWER = $@"
+            {ANSWER_CONTEXT}. {KEEP_ANSWER_SHORT}. {RESPOND_UNSURE_IF_YOUR}.
+            
+            Context: <CONTEXT_DATA/>.
+
+            Question: <INPUT_DATA/>.
+
             Answer:
         ";
 
         private static string CLASSIFIE_TEXT = @"
-            Classify the input into <LIST_COMMA_DATA>
-            Input: <INPUT_DATA>
+            Classify the input into <LIST_COMMA_DATA/>
+            Input: <INPUT_DATA/>
             Sentiment:
         ";
 
-        public static string ANSWER_CONTEXT = "Answer the question based on the context below:"; 
+        private static string SUMMARIZE = @"
+            Write a concise and comprehensive summary of:
+            <INPUT_DATA/>
+            Summery:
+        ";
 
-        public static string BOB_ASSISTANT = "Transcript of a dialog, where the User interacts with an Assistant named Bob. Bob is helpful, kind, honest, good at writing, and never fails to provide short answer to User's requests immediately and with precision.\r\nUser: Hello, Bob\r\nBob: Hello. How may I help you today?\r\nUser: Please tell me the largest city in Europe\r\nBob: Sure. The largest city in Europe is Moscow, the capital of Russia.";
-
-        public static string HELPFUL_ASSISTANT = "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\r\n\r\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.\r\n";
     }
 }
