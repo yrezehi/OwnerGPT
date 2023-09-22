300e271d0a38eb4ed60cd202455afb75a9bd5937
unknown <yrezehi@gmail.com>
2023-09-21 20:29:30 -0700

    add: paginate querable

--- OwnerGPT.Core/Utilities/Extenstions/LinqExtensions.cs
+++ OwnerGPT.Core/Utilities/Extenstions/LinqExtensions.cs
@@ -5,8 +5,13 @@ namespace OwnerGPT.Core.Utilities.Extenstions
 {
     public static class LinqExtensions
     {
-        public static IQueryable<IEntity> ConditionalWhere<IEntity>(this DbSet<IEntity> source, bool condition, Expression<Func<IEntity, bool>> predicate) where IEntity : class => condition ? source.Where(predicate) : source;
+        public static IQueryable<IEntity> ConditionalWhere<IEntity>(this DbSet<IEntity> source, bool condition, Expression<Func<IEntity, bool>> predicate) where IEntity : class =>
+            condition ? source.Where(predicate) : source;
 
-        public static async Task<int> ConditionalCount<IEntity>(this DbSet<IEntity> source, Expression<Func<IEntity, bool>> predicate) where IEntity : class => predicate != null ? await source.Where(predicate).AsQueryable().CountAsync() : await source.CountAsync();
+        public static async Task<int> ConditionalCount<IEntity>(this DbSet<IEntity> source, Expression<Func<IEntity, bool>> predicate) where IEntity : class =>
+            predicate != null ? await source.Where(predicate).AsQueryable().CountAsync() : await source.CountAsync();
+
+        public static IQueryable<IEntity> PaginateQuerable<IEntity>(this IQueryable<IEntity> source, int page, int size) where IEntity : class =>
+            source.Skip(page).Take(size);
     }
 }
