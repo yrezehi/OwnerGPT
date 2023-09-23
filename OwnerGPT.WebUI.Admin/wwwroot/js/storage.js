d2e7aa8f1987d344f844b40127ecaae44b2f85c3
unknown <yrezehi@gmail.com>
2023-09-23 05:08:28 -0700

    setup: storage.js

---
+++ OwnerGPT.WebUI.Admin/wwwroot/js/storage.js
@@ -0,0 +1,21 @@
+﻿var storage = function () {
+
+    function setValue(key, value) {
+        window.localStorage.setItem(key, value);
+    }
+
+    function nuke(key) {
+        if (key) {
+            window.localStorage.clear();
+        } else {
+            window.localStorage.clear();
+        }
+    }
+    
+    return function () {
+        return Object.freeze({
+            setValue: setValue,
+            nuke: nuke
+        });
+    }();
+}();
