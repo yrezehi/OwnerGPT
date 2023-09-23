a5de87c9c8654180cd294dd288e12117bd66dfea
unknown <yrezehi@gmail.com>
2023-09-23 00:56:33 -0700

    enhance: globals.js

--- OwnerGPT.WebUI.Admin/wwwroot/js/globals.js
+++ OwnerGPT.WebUI.Admin/wwwroot/js/globals.js
@@ -1,14 +1,25 @@
 ﻿var globalProperties = function () {
+
     var properties = {};
 
     function setProperty(key, value) {
-        setProperty = { ...properties, [key]: value };
+        properties = { ...properties, [key]: value };
+    }
+
+    function getProperty(key) {
+
+        if (!properties.hasOwnProperty(key)) {
+            notification.error(`global property named ${key} can't be found!`);
+            return;
+        }
+
+        return properties[key];
     }
 
     return function () {
-        return Object.freeze({
+        return {
             set: setProperty,
-            ...properties
-        });
+            get: getProperty,
+        };
     }();
 }();
