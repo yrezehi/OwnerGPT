b8536730d262b911d868d4f116724333bb29987f
unknown <yrezehi@gmail.com>
2023-09-23 08:33:28 -0700

    add: dom.js

---
+++ OwnerGPT.WebUI.Admin/wwwroot/js/dom.js
@@ -0,0 +1,14 @@
+﻿var doom = function () {
+
+    function createElementWithIdentifier(prefix, serlizedElement) {
+        var identifier = prefix + "-" + utility.shortGUID();
+        chatDialogContainer.innerHTML += serlizedElement;
+        return document.querySelector(`#${identifier}`);
+    }
+
+    return function () {
+        return Object.freeze({
+            createUniqueElement: createElementWithIdentifier
+        });
+    }();
+}();
