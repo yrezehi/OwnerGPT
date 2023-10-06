ab21359e702f80bc0eb6a21f9c9b3af4b138d95e
unknown <yrezehi@gmail.com>
2023-10-05 23:36:48 -0700

    setup: CrawlersCoordinator

---
+++ OwnerGPT.Plugins.WEB/Crawlers/CrawlersCoordinator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Concurrent;
+
+namespace OwnerGPT.Plugins.Parsers.Web.Crawlers
+{
+    public class CrawlersCoordinator
+    {
+        private readonly IHttpClientFactory HttpClientFactory;
+        private readonly ConcurrentDictionary<string, List<string>> ProgressiveJobs;
+
+        public CrawlersCoordinator(IHttpClientFactory httpClientFactory) =>
+            (HttpClientFactory, ProgressiveJobs) = (httpClientFactory, new());
+
+        public HttpClient GetClient() => 
+            HttpClientFactory.CreateClient();
+    }
+}
