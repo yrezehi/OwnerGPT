29aa95de1f7fb0e581c612f7e540536b76453400
unknown <yrezehi@gmail.com>
2023-10-05 23:28:53 -0700

    setup: CrawlersCoordinator

---
+++ OwnerGPT.Plugins.WEB/Crawlers/Coordinator.cs
@@ -0,0 +1,10 @@
+﻿namespace OwnerGPT.Plugins.Parsers.Web.Crawlers
+{
+    public class CrawlersCoordinator
+    {
+        private IHttpClientFactory HttpClientFactory;
+
+        public CrawlersCoordinator(IHttpClientFactory httpClientFactory) =>
+            HttpClientFactory = httpClientFactory;
+    }
+}
