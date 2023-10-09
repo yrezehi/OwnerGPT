9a17c9db382349f28a1a0161ab1bfe7b1c34fc11
unknown <yrezehi@gmail.com>
2023-10-09 08:01:46 -0700

    add: LoadAsCSV

--- OwnerGPT.Plugins.Parsers.Excel/Loader/LoadExcel.cs
+++ OwnerGPT.Plugins.Parsers.Excel/Loader/LoadExcel.cs
@@ -8,6 +8,16 @@ namespace OwnerGPT.Plugins.Parsers.Excel.Loader
         public async static Task<List<dynamic?>> Load(IFormFile file) =>
             MiniExcel.Query(await file.AsStream()).ToList();
 
+        public async static Task<IEnumerable<dynamic?>> LoadAsCSV(IFormFile file)
+        {
+            var csvStream = new MemoryStream();
+
+            MiniExcel.ConvertXlsxToCsv(await file.AsStream(), csvStream);
+            csvStream.Position = 0;
+
+            return MiniExcel.Query(csvStream);
+        }
+
         private async static Task<Stream> AsStream(this IFormFile file)
         {
             var stream = new MemoryStream();
