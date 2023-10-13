02c111c8bad8830d356de2a782ce6a2be4e08b0f
unknown <yrezehi@gmail.com>
2023-10-13 07:30:39 -0700

    setup: word plugin

---
+++ OwnerGPT.Plugins.Parsers.Word/WordPlugin.cs
@@ -0,0 +1,11 @@
+﻿using OwnerGPT.Plugins.Manager.Interfaces;
+
+namespace OwnerGPT.Plugins.Parsers.Word
+{
+    public class WordPlugin : IOwnerGPTParserPlugin<string, string>
+    {
+        public static string Process(byte[] file) =>
+            "";
+        
+    }
+}
